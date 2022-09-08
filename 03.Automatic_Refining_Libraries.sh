360a618a27df9811cf27f196a6bfbc6e03a3f65b
Jacopo Martelossi <62595944+jacopoM28@users.noreply.github.com>
2022-09-08 21:14:04 +0200

    Update 03.Automatic_Refining_Libraries.sh

--- 03.Automatic_Refining_Libraries.sh
+++ 03.Automatic_Refining_Libraries.sh
@@ -11,21 +11,24 @@
 
 for i in *Renamed.fa; do 
 
-blastx -query "$i" -db Cleaned_Prot.pep -evalue 1e-10 -num_descriptions 10 -out "$i"_Blastx.out -num_threads 20; 
+  blastx -query "$i" -db Cleaned_Prot.pep -evalue 1e-10 -num_descriptions 10 -out "$i"_Blastx.out -num_threads 20; 
+  
 done;
 
 ##2.
 for i in *Blastx.out; do 
 
-var1=$( echo "$i" | awk -F"-" '{print$1}'); 
-perl /media/storage/jacopomartelossi/Software/ProtExcluder1.1/ProtExcluder.pl "$i" "$var1"-families_Renamed.fa; 
+  var1=$( echo "$i" | awk -F"-" '{print$1}'); 
+  perl /media/storage/jacopomartelossi/Software/ProtExcluder1.1/ProtExcluder.pl "$i" "$var1"-families_Renamed.fa; 
+  
 done;
 
 ##3.
 
 for i in *fanoProtFinal; do 
 
-var1=$( echo "$i" | awk -F"-" '{print$1}'); 
-perl /media/storage/jacopomartelossi/Software/cleanup_tandem.pl -f "$i" -minlen 80 -misschar n -nr 0.5 > "$var1"-families.noProt
+  var1=$( echo "$i" | awk -F"-" '{print$1}'); 
+  perl /media/storage/jacopomartelossi/Software/cleanup_tandem.pl -f "$i" -minlen 80 -misschar n -nr 0.5 > "$var1"-families.noProt
 .noTandem.fa; 
+
 done;
