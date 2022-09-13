26948520e2a093fc3edfbcd93aa3e2175299cff6
Jacopo Martelossi <62595944+jacopoM28@users.noreply.github.com>
2022-09-14 00:25:43 +0200

    Update 02.RepeatMasker.sh

--- 02.RepeatMasker.sh
+++ 02.RepeatMasker.sh
@@ -1,7 +1,7 @@
 ########################################################################
 #                 TE annotation with raw TE libraries                  #
 ########################################################################
-#The script must be run in a directory with all genomes (*.fa files) and all libraries (*-families_renamed.fa)
+#The script must be run in a directory with all genomes in fasta files (*.fa files) and all libraries (*-families_renamed.fa)
 
 for i in *genomic.fa; do 
   varSpecie=$( echo "$i" | cut -d"." -f1); 
