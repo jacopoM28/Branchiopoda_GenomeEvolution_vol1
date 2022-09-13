e492bc9f70f963f1a38a01e9b93411761eeb1db8
Jacopo Martelossi <62595944+jacopoM28@users.noreply.github.com>
2022-09-14 00:30:53 +0200

    Delete 02.RepeatMasker.sh

--- 02.RepeatMasker.sh
+++
@@ -1,9 +0,0 @@
-########################################################################
-#                 TE annotation with raw TE libraries                  #
-########################################################################
-#The script must be run in a directory with all genomes in fasta files (*.fa files) and all libraries (*-families_renamed.fa)
-
-for i in *genomic.fa; do 
-  varSpecie=$( echo "$i" | cut -d"." -f1); 
-  RepeatMasker -small -e NCBI -lib "$varSpecie"-families_renamed.fa -pa 20 -gff -a -no_is -nolow -norna -s "$i";
-done;
