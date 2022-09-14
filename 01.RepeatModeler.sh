a2b9a11f46403d1a3210941b8d287e6f5bcf7e07
Jacopo Martelossi <62595944+jacopoM28@users.noreply.github.com>
2022-09-14 10:46:33 +0200

    Update 01.RepeatModeler.sh

--- 01.RepeatModeler.sh
+++ 01.RepeatModeler.sh
@@ -1,8 +1,7 @@
 ########################################################################
 #                      De novo TE library                              #
 ########################################################################
-#The script must be run in a directory with all genomes in fasta files (*.fa files)
-#In my case, I have: Tusa.genomic.fa; Tita.genomic.fa ecc...
+##The script must be run in a directory with all genomes in fasta files (*.fa files)
 
 NINJA_DIR=/home/jacopomartelossi/.conda/envs/Repeats/bin
 for i in *.fa; do 
