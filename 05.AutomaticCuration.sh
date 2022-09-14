99834fa02ab02c275340d0e8f53a2862b4e78de9
Jacopo Martelossi <62595944+jacopoM28@users.noreply.github.com>
2022-09-14 10:45:08 +0200

    Update 05.AutomaticCuration.sh

--- 05.AutomaticCuration.sh
+++ 05.AutomaticCuration.sh
@@ -1,7 +1,7 @@
 ################################################################################
 #                  Automatic curation of raw TE libraries                      #
 ################################################################################
-#This script will use an automatic versione of the BEE pipeline to extend raw consensus.
+##Automatic versione of the BEE pipeline to extend raw consensus.
 
 for i in ../../Genomes/Branchiopods/*genomic.fa; do
 
@@ -14,7 +14,7 @@ for i in ../../Genomes/Branchiopods/*genomic.fa; do
                 mkdir rnd"$j";
                 cd rnd"$j";
                 ln -s ../../../../Genomes/Branchiopods/"$i"
-                #If it's the first extension round, use the automatically generated libraries without genes and tandem repeats as queries...
+                #If it's the first extension round, use the automatically generated libraries as queries...
                 if [ "$j" = 1 ]; then
                         ln -s  ../../../Libs/"$varSpecie"-families.noProt.noTandem.fa
                         python ../../../../TE_Scripts/AutomaticBEE.py --genome "$varSpecie".genomic.fa --lib "$varSpecie"-families.noProt.noTandem.fa --out "$varSpecie"_auto_rnd"$j" --blast_identity 70 --blast_query_cov 70 --num_threads 30 --min_Blast_Hits 5
@@ -22,10 +22,11 @@ for i in ../../Genomes/Branchiopods/*genomic.fa; do
                 else
                         #Previous round variable, necessary to softlink the correct library as new query
                         varRound="$(( j-1 ))"
-                        ln -s ../rnd"$varRound"/"$varSpecie"_auto_rnd"$varRound"
-                        python ../../../../TE_Scripts/AutomaticBEE.py --genome "$varSpecie".genomic.fa --lib "$varSpecie"_auto_rnd"$varRound" --out "$varSpecie"_auto_rnd"$j" --blast_identity 70 --blast_query_cov 70 --num_threads 30 --min_Blast_Hits 5;
+                        ln -s ../rnd"$varRound"/"$varSpecie"_auto_rnd"$varRound"_Consensus.fa
+                        python ../../../../TE_Scripts/AutomaticBEE.py --genome "$varSpecie".genomic.fa --lib "$varSpecie"_auto_rnd"$varRound"_Consensus.fa --out "$varSpecie"_auto_rnd"$j" --blast_identity 70 --blast_query_cov 70 --num_threads 30 --min_Blast_Hits 5;
                 fi;
                 cd ../;
         done;
         cd ../
 done;
+
