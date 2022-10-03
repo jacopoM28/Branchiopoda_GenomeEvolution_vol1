c631fcda48be70734f16931fd6fe4ef2b9973e78
Jacopo Martelossi <62595944+jacopoM28@users.noreply.github.com>
2022-10-03 15:44:18 +0200

    Update 00.Rename_RepeatPepsLib.sh

--- 00.Rename_RepeatPepsLib.sh
+++ 00.Rename_RepeatPepsLib.sh
@@ -1,10 +1,10 @@
-##########################################################################
-#.   Rename the TE-related protein library at the superfamily level.     #
-##########################################################################
+#######################################################################################
+#.   Rename the RepeatMasker TE-related protein library at the superfamily level.     #
+#######################################################################################
 
 while read line; do 
   var1=$( echo "$line" | awk '{print$1}'); 
-   var2=$( echo "$2" | awk '{print$2}'; sed -i 's,'"$var1"','"$var2"',
-g' RepeatPeps_Renamed.lib; 
-
+  var2=$( echo "$line" | awk '{print$2}'); 
+  sed -i 's,'"$var1"' ,'"$var2"' ,g' RepeatPeps_Renamed.lib; 
+  
 done< Rename_RepeatPeps.txt
